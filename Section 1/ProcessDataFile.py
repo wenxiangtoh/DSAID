3c4c80bcd0231d96feb9549f1cf5625b798097f5
elric <tohwenxiang94@gmail.com>
2020-11-07 11:18:57 +0800

    Added processDataFile function

--- Section 1/ProcessDataFile.py
+++ Section 1/ProcessDataFile.py
@@ -1,37 +1,40 @@
 # imported modules
 import pandas as pd
-import numpy as np
 
-df = pd.read_csv('dataset.csv')
 
-# Dropping row if name column is Nan
-df.dropna(subset=['name'], inplace=True)
+def processDatafile(file):
+    # Read dataset.csv using pandas module
+    df = pd.read_csv(file)
 
-# full_name data frame with split value columns
-full_name = df["name"].str.split(" ", n=1, expand=True)
-# Adding first_name column at position 0 using full_name data frame
-df.insert(0, "first_name", full_name[0])
-# Adding last_name column at position 1 using last_name data frame
-df.insert(1, "last_name", full_name[1])
-# Dropping name column
-df.drop(columns=["name"], inplace=True)
+    # Dropping row if name column is Nan
+    df.dropna(subset=['name'], inplace=True)
 
-# Variable to temporary store price df values
-nonZeroinFooter = []
-# Variable to temporary store above_100 df values
-above_100 = []
+    # full_name data frame with split value columns
+    full_name = df["name"].str.split(" ", n=1, expand=True)
+    # Adding first_name column at position 0 using full_name data frame
+    df.insert(0, "first_name", full_name[0])
+    # Adding last_name column at position 1 using last_name data frame
+    df.insert(1, "last_name", full_name[1])
+    # Dropping name column
+    df.drop(columns=["name"], inplace=True)
 
-for idx, val in enumerate(df['price']):
-    # Remove any zeros prepended to the price dataframe
-    nonZeroinFooter.append(str(val).lstrip('0.'))
-    if(val > 100):
-        # Append true to temporary variable above_100
-        above_100.append(str.lower("true"))
-    else:
-        # Append false to temporary variable above_100
-        above_100.append(str.lower("false"))
+    # Variable to temporary store price df values
+    nonZeroinFooter = []
+    # Variable to temporary store above_100 df values
+    above_100 = []
 
-df['price'] = nonZeroinFooter
-df["above_100"] = above_100
+    for idx, val in enumerate(df['price']):
+        # Remove any zeros prepended to the price dataframe
+        nonZeroinFooter.append(str(val).lstrip('0.'))
+        if(val > 100):
+            # Append true to temporary variable above_100
+            above_100.append(str.lower("true"))
+        else:
+            # Append false to temporary variable above_100
+            above_100.append(str.lower("false"))
 
-df.to_csv('processedDataSet.csv', index=False)
+    df['price'] = nonZeroinFooter
+    df["above_100"] = above_100
+
+    # Write updated df to procesedDataSet.csv
+    df.to_csv('processedDataSet.csv', index=False)
